8db6661cfadf219e95e8b0b8ef1a4b3c2a475705
fahad_khatri <muhammadfahad1393@gmail.com>
2022-09-20 20:49:47 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Fahad
+          Learn React Fahad Farooq
         </a>
       </header>
     </div>
